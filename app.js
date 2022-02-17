79b361c1c112a7bb8811e0b5a721748f5a76f3a3
LiamSL <liamleung@outlook.com>
2022-02-17 14:35:12 -0500

    actually done

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 3
         });
-        $('.js-add-slide').on('click', function() {
+        $('.addSlide').on('click', function() {
         slideIndex++;
         $('.add-remove').slick('slickAdd','<div><h3>' + slideIndex + '</h3></div>');
         });
         
-        $('.js-remove-slide').on('click', function() {
+        $('.removeSlide').on('click', function() {
         $('.add-remove').slick('slickRemove',slideIndex - 1);
         if (slideIndex !== 0){
             slideIndex--;
